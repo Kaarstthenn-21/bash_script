67a0bc9069db04c38ffb6c48f1435cb15abbbce8
Kaarstthenn-21 <kaarstthen.ancco@tecsup.edu.pe>
2023-06-09 19:43:39 -0500

    Refactor: Ec2 Script ClI deploy

--- BashScript/Ec2_Scripts/full-deploy.sh
+++ BashScript/Ec2_Scripts/full-deploy.sh
@@ -1,7 +1,8 @@
-
 source ./variables.sh
 
-
+# **************************************************************************************
+#                                     Configure Security group 22/80/443
+# **************************************************************************************
 if aws ec2 describe-security-groups --group-names "$GROUP_NAME" >/dev/null 2>&1; then
   echo "El grupo de seguridad $GROUP_NAME ya existe. No es necesario crearlo."
 else
@@ -17,7 +18,6 @@ else
 fi
 
 GROUP_DESCRIPTION=$(aws ec2 describe-security-groups --group-names "$GROUP_NAME")
-
 
 if echo "$GROUP_DESCRIPTION" | grep -q "\"FromPort\": $PORT_SSH,"; then
   echo "El puerto $PORT_SSH está configurado en el grupo de seguridad $GROUP_NAME."
@@ -57,6 +57,10 @@ else
   echo "Se ha habilitado el acceso HTTPS (puerto 443) en el grupo de seguridad $GROUP_ID ($GROUP_NAME)."
 fi
 
+# **************************************************************************************
+#                                     Create Instance
+# **************************************************************************************
+
 # Crear la instancia de EC2
 INSTANCE_ID=$(aws ec2 run-instances \
   --region $AWS_REGION \
@@ -87,15 +91,64 @@ echo "La instancia de EC2 ($INSTANCE_ID) tiene la dirección IP pública: $PUBLI
 # Esperar hasta que la instancia esté en estado 'running' antes de conectarse por SSH
 aws ec2 wait instance-status-ok --region $AWS_REGION --instance-ids $INSTANCE_ID
 
-#Creamos Ip elastica
-allocation=$(aws ec2 allocate-address --domain vpc)
+# **************************************************************************************
+#                                     Assign new Volume
+# **************************************************************************************
+# Obtener el ID del volumen EBS asociado a la instancia
+VOLUME_ID=$(aws ec2 describe-volumes --filters "Name=attachment.instance-id,Values=$INSTANCE_ID" --query "Volumes[0].VolumeId" --output text)
 
-readarray -t ip_address < <(aws ec2 allocate-address --domain vpc --query 'PublicIp' --output json)
+# Detener la instancia EC2
+aws ec2 stop-instances --instance-ids $INSTANCE_ID
 
-#Asociar Ip elastica a Instancia Ec2
-aws ec2 associate-address --instance-id "$INSTANCE_ID" --public-ip "${ip_address[0]}"
+# Esperar hasta que la instancia se detenga
+echo "Esperando hasta que la instancia se detenga..."
+aws ec2 wait instance-stopped --instance-ids $INSTANCE_ID
 
-# Conectar por SSH a la instancia
-echo "Conectándose por SSH a la instancia de EC2 ($INSTANCE_ID)..."
-ssh -i C:/Users/KAARSTTHENN/PEM/$KEY_PAIR_NAME.pem ec2-user@$PUBLIC_IP
+# Modificar el tamaño del volumen EBS
+aws ec2 modify-volume --volume-id $VOLUME_ID --size $NEW_SIZE
 
+# Modificar el tipo del volumen EBS
+aws ec2 modify-volume --volume-id $VOLUME_ID --volume-type $NEW_VOLUME_TYPE
+
+# Iniciar la instancia EC2 nuevamente
+aws ec2 start-instances --instance-ids $INSTANCE_ID
+
+# Esperar hasta que la instancia se inicie
+echo "Esperando hasta que la instancia se inicie..."
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+echo "La configuración del volumen EBS ($VOLUME_ID) ha sido modificada exitosamente."
+
+# **************************************************************************************
+#                                     Assign elastic IP
+# **************************************************************************************
+# Obtener una dirección IP elástica disponible
+AVAILABLE_IP=$(aws ec2 describe-addresses --filters "Name=instance-id,Values=NULL" --query 'Addresses[0].PublicIp' --output text)
+
+# Comprobar si existe una dirección IP elástica disponible
+AVAILABLE_IP=$(aws ec2 describe-addresses --query 'Addresses[?AssociationId==null]' --output text)
+if [[ -z $AVAILABLE_IP ]]; then
+  # Crear una nueva dirección IP elástica y asignarla a la instancia EC2
+  echo "No se encontró una dirección IP elástica disponible. Creando una nueva dirección IP..."
+  ALLOCATE_IP=$(aws ec2 allocate-address --domain vpc --query 'PublicIp' --output text)
+  echo "La dirección IP $ALLOCATE_IP se ha creado correctamente."
+
+  echo "Asignando la dirección IP $new_ip a la instancia $INSTANCE_ID..."
+  aws ec2 associate-address --instance-id "$INSTANCE_ID" --public-ip "$ALLOCATE_IP"
+  echo "La dirección IP $ALLOCATE_IP se ha asignado correctamente a la instancia $INSTANCE_ID."
+  sleep 45
+  # Conectar por SSH a la instancia+
+  echo "Conectándose por SSH a la instancia de EC2 ($INSTANCE_ID)..."
+  ssh -i C:/Users/KAARSTTHENN/PEM/$KEY_PAIR_NAME.pem ec2-user@$ALLOCATE_IP
+else
+  echo "Se encontró una dirección IP elástica disponible: $allocation"
+  # Asignar la dirección IP disponible a la instancia EC2
+  echo "Asignando la dirección IP $AVAILABLE_IP a la instancia $INSTANCE_ID..."
+  aws ec2 associate-address --instance-id "$INSTANCE_ID" --public-ip "$AVAILABLE_IP"
+  echo "La dirección IP $AVAILABLE_IP se ha asignado correctamente a la instancia $INSTANCE_ID."
+
+  sleep 45
+  # Conectar por SSH a la instancia+
+  echo "Conectándose por SSH a la instancia de EC2 ($INSTANCE_ID)..."
+  ssh -i C:/Users/KAARSTTHENN/PEM/$KEY_PAIR_NAME.pem ec2-user@$AVAILABLE_IP
+fi
--- BashScript/Ec2_Scripts/variables.sh
+++ BashScript/Ec2_Scripts/variables.sh
@@ -9,12 +9,63 @@ INSTANCE_TYPE="t2.micro"
 KEY_PAIR_NAME="apache-key"
 SECURITY_GROUP_NAME=$GROUP_NAME
 AMI_ID="ami-0715c1897453cabd1"
-APACHE_INSTALL_SCRIPT="#!/bin/bash
-                 yum update -y
-                 yum install -y httpd
-                 echo 'Bootcamp AWS Ignite 2023 Apache!' > /var/www/html/index.html
-                 systemctl start httpd
-                 systemctl enable httpd"
+APACHE_INSTALL_SCRIPT="\#\!/bin/bash\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ yum\ update\ -y\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ yum\ install\ -y\ httpd\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ echo\ \'Bootcamp\ AWS\ Ignite\ 2023\ Apache\!\'\ \>\ /var/www/html/index.html\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ systemctl\ start\ httpd\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ systemctl\ enable\ httpd\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Rules\ firewall\n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ firewall-cmd\ --zone=public\ --add-service=http\ --permanent\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ firewall-cmd\ --zone=public\ --add-service=https\ --permanent\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ firewall-cmd\ --reload\ \ \#\ Recargar\ las\ reglas\ de\ firewall\n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Configure\ SSL/TLS\ let\'s\ encrypt\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ amazon-linux-extras\ install\ epel\ -y\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ yum\ install\ certbot\ python2-certbot-apache\ -y\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ certbot\ --apache\ --non-interactive\ --agree-tos\ --email\ tu-email@ejemplo.com\ --domains\ tu-dominio.com\n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#Restart\ Server\n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ systemctl\ restart\ httpd\ \ \ \n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Configure\ Security\ module\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ yum\ install\ mod_security\ -y\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ echo\ \"Include\ /etc/httpd/conf.d/mod_security.conf\"\ \>\>\ /etc/httpd/conf/httpd.conf\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ cat\ \<\<\ EOF\ \>\ /etc/httpd/conf.d/mod_security.conf\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRuleEngine\ On\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRequestBodyAccess\ On\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRule\ REQUEST_HEADERS:Content-Type\ \"text/xml\"\ \"id:\'200000\'\,phase:1\,t:none\,t:lowercase\,pass\,nolog\,ctl:requestBodyProcessor=XML\"\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRequestBodyLimit\ 10000000\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRequestBodyNoFilesLimit\ 64000\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRequestBodyInMemoryLimit\ 64000\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRequestBodyLimitAction\ Reject\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRule\ REQBODY_ERROR\ \"\!@eq\ 0\"\ \"id:\'200001\'\,phase:2\,t:none\,log\,deny\,status:400\,msg:\'XML\ request\ body\ is\ larger\ than\ allowed\'\"\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRule\ REQUEST_LINE\ \"\!\(OPTIONS\|GET\|HEAD\|POST\|PUT\|DELETE\|CONNECT\|TRACE\|PATCH\)\"\ \"id:\'200002\'\,phase:2\,t:none\,log\,deny\,status:405\,msg:\'Method\ is\ not\ allowed\'\"\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ SecRule\ REQUEST_HEADERS:User-Agent\ \"\(libwww-perl\|curl\|wget\|python\|nikto\|sqlmap\)\"\ \"id:\'200003\'\,phase:2\,t:none\,log\,deny\,status:400\,msg:\'User-Agent\ is\ not\ allowed\'\"\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ EOF\n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Reiniciar\ servicio\ de\ Apache\ para\ aplicar\ los\ cambios\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ systemctl\ restart\ httpd\ \n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Configurar\ seguridad\ de\ Apache\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ echo\ \"ServerTokens\ Prod\"\ \>\>\ /etc/httpd/conf/httpd.conf\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ echo\ \"ServerSignature\ Off\"\ \>\>\ /etc/httpd/conf/httpd.conf\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Configurar\ módulo\ correspondiente\ a\ PHP\ \(ejemplo\)\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ yum\ install\ php\ php-mysql\ -y\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ systemctl\ restart\ httpd\n\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \#\ Configurar\ módulo\ correspondiente\ a\ Node.js\ \(ejemplo\)\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ curl\ -sL\ https://rpm.nodesource.com/setup_14.x\ \|\ bash\ -\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ yum\ install\ nodejs\ -y\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ systemctl\ restart\ httpd"
+
+# "#!/bin/bash
+#                  yum update -y
+#                  yum install -y httpd
+#                  echo 'Bootcamp AWS Ignite 2023 Apache!' > /var/www/html/index.html
+#                  systemctl start httpd
+#                  systemctl enable httpd
+
+#                  # Rules firewall
+
+#                  firewall-cmd --zone=public --add-service=http --permanent
+#                  firewall-cmd --zone=public --add-service=https --permanent
+#                  firewall-cmd --reload  # Recargar las reglas de firewall
+
+#                  # Configure SSL/TLS let's encrypt
+#                  amazon-linux-extras install epel -y
+#                  yum install certbot python2-certbot-apache -y
+#                  certbot --apache --non-interactive --agree-tos --email tu-email@ejemplo.com --domains tu-dominio.com
+
+#                  #Restart Server
+
+#                  systemctl restart httpd
+
+#                  # Configure Security module
+#                  yum install mod_security -y
+#                  echo "Include /etc/httpd/conf.d/mod_security.conf" >> /etc/httpd/conf/httpd.conf
+
+#                  cat << EOF > /etc/httpd/conf.d/mod_security.conf
+#                  SecRuleEngine On
+#                  SecRequestBodyAccess On
+#                  SecRule REQUEST_HEADERS:Content-Type "text/xml" "id:'200000',phase:1,t:none,t:lowercase,pass,nolog,ctl:requestBodyProcessor=XML"
+#                  SecRequestBodyLimit 10000000
+#                  SecRequestBodyNoFilesLimit 64000
+#                  SecRequestBodyInMemoryLimit 64000
+#                  SecRequestBodyLimitAction Reject
+#                  SecRule REQBODY_ERROR "!@eq 0" "id:'200001',phase:2,t:none,log,deny,status:400,msg:'XML request body is larger than allowed'"
+#                  SecRule REQUEST_LINE "!(OPTIONS|GET|HEAD|POST|PUT|DELETE|CONNECT|TRACE|PATCH)" "id:'200002',phase:2,t:none,log,deny,status:405,msg:'Method is not allowed'"
+#                  SecRule REQUEST_HEADERS:User-Agent "(libwww-perl|curl|wget|python|nikto|sqlmap)" "id:'200003',phase:2,t:none,log,deny,status:400,msg:'User-Agent is not allowed'"
+#                  EOF
+
+#                  # Reiniciar servicio de Apache para aplicar los cambios
+#                  systemctl restart httpd
+
+#                  # Configurar seguridad de Apache
+#                  echo "ServerTokens Prod" >> /etc/httpd/conf/httpd.conf
+#                  echo "ServerSignature Off" >> /etc/httpd/conf/httpd.conf
+
+#                  # Configurar módulo correspondiente a PHP (ejemplo)
+#                  yum install php php-mysql -y
+#                  systemctl restart httpd
+
+#                  # Configurar módulo correspondiente a Node.js (ejemplo)
+#                  curl -sL https://rpm.nodesource.com/setup_14.x | bash -
+#                  yum install nodejs -y
+#                  systemctl restart httpd
+#                  "
 
 #Variables para obtener Ip actual
 MY_IP=$(curl -s http://checkip.amazonaws.com)
@@ -23,3 +74,7 @@ MY_IP=$(curl -s http://checkip.amazonaws.com)
 PORT_SSH=22
 PORT_HTTP=80
 PORT_HTTPS=443
+
+#Variables para EBS
+NEW_SIZE=10
+NEW_VOLUME_TYPE="gp2"
